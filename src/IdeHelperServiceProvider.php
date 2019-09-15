d1f31d6f4ba6cc638b9814c26a7e0188c9776a5c
Robin Radic <rradic@hotmail.com>
2019-09-15 11:38:05 +0200

    upd

---
+++ src/IdeHelperServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pyradic\IdeHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class IdeHelperServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
